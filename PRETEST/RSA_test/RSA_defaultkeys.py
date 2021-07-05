950abebd84f36b722cc49cfd0adb80728609cc67
YunHsiuLu <yulu@YunHsiuLudeMacBook-Pro.local>
2021-07-05 20:34:54 +0800

    update RSA_defaultkeys

--- PRETEST/RSA_test/RSA_defaultkeys.py
+++ PRETEST/RSA_test/RSA_defaultkeys.py
@@ -8,6 +8,8 @@ import random
 import numpy as np
 import matplotlib.pyplot as plt
 
+save = False
+
 # RSA512 for L = 5
 #h = SlidingWindows(KEY512.pri, 5)
 h = ['10111', '0', '11011', '10001', '11000', '11100', '10111', '10111', '000', '11010', '00000', '10000', '0', '10111', '000', '10110', '11010', '000', '11101', '10000', '0', '10000', '11010', '0', '11010', '0', '11111', '00', '11000', '0', '10101', '11010', '0', '10011', '10110', '10011', '0', '10111', '11110', '10101', '000', '11010', '00', '11111', '10101', '0', '11001', '10000', '11111', '10101', '00', '11011', '0000', '11000', '10100', '0', '10011', '11011', '000000', '10011', '0', '10110', '10100', '000', '11110', '0', '10011', '11110', '11111', '10000', '0', '11101', '11011', '11010', '11000', '00', '11100', '11100', '0', '10100', '0', '11010', '000', '11011', '0', '10001', '10101', '00', '10101', '000', '11000', '10010', '11011', '10010', '000', '11111', '0', '11101', '11000', '11000', '10010', '000000', '10001', '0', '11000', '10111', '00', '11000', '000', '10110', '10001', '0', '10100', '10011', '10101', '0', '11011', '11010', '0', '10100', '000', '10001', '11100', '0', '10010', '00', '11001']
@@ -96,11 +98,11 @@ y = np.array(y)
 label = ['c1', 'c2', 'c3', 'c4', 'c5']
 color = ['r', 'g', 'b', 'k', 'c']
 
-fig, axs = plt.subplots(5, 1, sharex=True, sharey=True, figsize = (12, 7))
+fig, axs = plt.subplots(5, 1, sharex=True, sharey=True, figsize = (12, 12))
 fig.suptitle('Decrypt a set of ciphertext each window execution time')
 axs[-1].set_xlabel('.', color=(0,0,0,0))
 axs[-1].set_ylabel('.', color=(0,0,0,0))
-fig.text(0.5, 0.02, 'Window No.', ha='center')
+fig.text(0.5, 0.01, 'Window No.', ha='center')
 fig.text(0.01, 0.5, 'Execution Time (ns)', va='center', rotation='vertical')
 from matplotlib import ticker
 formatter = ticker.ScalarFormatter(useMathText=True)
@@ -115,9 +117,18 @@ for a in axs.flat:
 	a.label_outer()
 plt.tight_layout()
 
+if save:
+	plt.savefig('Multi_WindowExTime.pdf')
+	plt.savefig('Multi_WindowExTime.png')
+else:
+	plt.show()
+'''
+plt.figure(figsize=(7, 2))
+plt.title('Decrypt 1 ciphertext each window execution time')
+plt.bar(x, y[0], color='r')
+plt.xlabel('Window No.')
+plt.ylabel('Execution Time (ns)')
+plt.savefig('Single_WindowExTime.pdf')
+plt.savefig('Single_WindowExTime.png')
+'''
 
-plt.savefig('Multi_WindowExTime.pdf')
-plt.savefig('Multi_WindowExTime.png')
-
-
-#plt.show()
